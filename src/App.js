c8c30c5858e4af8f7a3dd5d5669799952e28ce54
hoodwinker13 <euhan72@gmail.com>
2022-01-24 14:20:51 -0800

    switched up login; screen folder isn't used anymore

--- src/App.js
+++ src/App.js
@@ -1,187 +1,77 @@
-import * as React from 'react';
-import { Button, Text, StyleSheet, TextInput, View } from 'react-native';
-import { getFocusedRouteNameFromRoute, NavigationContainer, NavigationHelpersContext } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React, { useState, useEffect } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import "./App.css";
+import Routes from "./Routes";
+import Nav from "react-bootstrap/Nav";
+import { LinkContainer } from "react-router-bootstrap";
+import { AppContext } from "./lib/contextLib";
+import { Auth } from "aws-amplify";
+import { useHistory } from "react-router-dom";
+import { onError } from "./lib/errorLib";
 
-import SignUpScreen from "./screens/SignUp";
+function App() {
+  const history = useHistory();
 
-const styles = StyleSheet.create ({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    input: {
-        height: 40,
-        margin: 12,
-        borderWidth: 1,
-        padding: 10,
-    },
-});
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
 
-const AuthContext = React.createContext();
-
-function SplashScreen() {
-  return (
-    <View style = {styles.container}>
-      <Text>Loading...</Text>
-    </View>
-  );
-}
-
-function HomeScreen({ navigation }) {
-    const { signOut } = React.useContext(AuthContext);
-
-    React.useLayoutEffect(() => {
-        navigation.setOptions({
-            headerRight: () => (
-                <Button onPress = { signOut } title = "Sign Out" />
-            )
-        })
-    })
+  useEffect(() => {
+    onLoad();
+  }, []);
   
-    return (
-    <View style = {styles.container}>
-      <Text>Signed in!</Text>
-    </View>
-  );
-}
+  async function onLoad() {
+    try {
+      await Auth.currentSession();
+      userHasAuthenticated(true);
+    }
+    catch(e) {
+      if (e !== 'No current user') {
+        onError(e);
+      }
+    }
+  
+    setIsAuthenticating(false);
+  }
 
-function SignInScreen() {
-  const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
-
-  const { signIn } = React.useContext(AuthContext);
+  async function handleLogout() {
+    await Auth.signOut();
+  
+    userHasAuthenticated(false);
+  
+    history.push("/login");
+  }
 
   return (
-    <View style = {styles.container}>
-      <TextInput
-        style = {styles.input}
-        placeholder="Username"
-        value={username}
-        onChangeText={setUsername}
-      />
-      <TextInput
-        style = {styles.input}
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
-      <Button title="Sign in" onPress={() => signIn({ username, password })} />
-      <Button title = "Sign Up" onPress = {() => navigation.navigate( SignUpScreen ) } />
-    </View>
+    <div className="App container py-3">
+      <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
+        <LinkContainer to="/">
+          <Navbar.Brand className="font-weight-bold text-muted">
+            Scratch
+          </Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav activeKey={window.location.pathname}>
+            {isAuthenticated ? (
+              <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+            ) : (
+              <>
+                <LinkContainer to="/signup">
+                  <Nav.Link>Signup</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/login">
+                  <Nav.Link>Login</Nav.Link>
+                </LinkContainer>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
+        <Routes />
+      </AppContext.Provider>
+    </div>
   );
 }
 
-const Stack = createStackNavigator();
-
-export default function App({ navigation }) {
-
-    const [state, dispatch] = React.useReducer(
-    (prevState, action) => {
-        switch (action.type) {
-        case 'RESTORE_TOKEN':
-            return {
-            ...prevState,
-            userToken: action.token,
-            isLoading: false,
-            };
-        case 'SIGN_IN':
-            return {
-            ...prevState,
-            isSignout: false,
-            userToken: action.token,
-            };
-        case 'SIGN_OUT':
-            return {
-            ...prevState,
-            isSignout: true,
-            userToken: null,
-            };
-        }
-    },
-    {
-        isLoading: true,
-        isSignout: false,
-        userToken: null,
-    }
-    );
-
-    React.useEffect(() => {
-    // Fetch the token from storage then navigate to our appropriate place
-    const bootstrapAsync = async () => {
-        let userToken;
-
-        try {
-        // Restore token stored in `SecureStore` or any other encrypted storage
-        // userToken = await SecureStore.getItemAsync('userToken');
-        } catch (e) {
-        // Restoring token failed
-        }
-
-        // After restoring token, we may need to validate it in production apps
-
-        // This will switch to the App screen or Auth screen and this loading
-        // screen will be unmounted and thrown away.
-        dispatch({ type: 'RESTORE_TOKEN', token: userToken });
-    };
-
-    bootstrapAsync();
-    }, []);
-
-    const authContext = React.useMemo(
-    () => ({
-        signIn: async (data) => {
-        // In a production app, we need to send some data (usually username, password) to server and get a token
-        // We will also need to handle errors if sign in failed
-        // After getting token, we need to persist the token using `SecureStore` or any other encrypted storage
-        // In the example, we'll use a dummy token
-
-        dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
-        },
-        signOut: () => dispatch({ type: 'SIGN_OUT' }),
-        signUp: async (data) => {
-        // In a production app, we need to send user data to server and get a token
-        // We will also need to handle errors if sign up failed
-        // After getting token, we need to persist the token using `SecureStore` or any other encrypted storage
-        // In the example, we'll use a dummy token
-
-        dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
-        },
-    }),
-    []
-    );
-
-    const { signOut } = () => React.useContext(AuthContext);
-
-    return (
-    <AuthContext.Provider value={authContext}>
-        <NavigationContainer>
-        <Stack.Navigator>
-            {state.isLoading ? (
-            // We haven't finished checking for the token yet
-            <Stack.Screen name="Splash" component={SplashScreen} />
-            ) : state.userToken == null ? (
-            // No token found, user isn't signed in
-            <Stack.Screen
-                name="SignIn"
-                component={SignInScreen}
-                options={{
-                title: 'Sign in',
-                // When logging out, a pop animation feels intuitive
-                animationTypeForReplace: state.isSignout ? 'pop' : 'push',
-                }}
-            />
-            ) : (
-            // User is signed in
-            <Stack.Screen 
-                name="Home" 
-                component={HomeScreen} 
-            />
-            )}
-        </Stack.Navigator>
-        </NavigationContainer>
-    </AuthContext.Provider>
-    );
-}
+export default App;
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,18 +1,39 @@
 import React, { useState } from "react";
+import { Auth } from "aws-amplify";
 import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+import { useHistory } from "react-router-dom";
+import LoaderButton from "../components/LoaderButton";
+import { useAppContext } from "../lib/contextLib";
+import { useFormFields } from "../lib/hooksLib";
+import { onError } from "../lib/errorLib";
 import "./Login.css";
 
 export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const history = useHistory();
+  const { userHasAuthenticated } = useAppContext();
+  const [isLoading, setIsLoading] = useState(false);
+  const [fields, handleFieldChange] = useFormFields({
+    email: "",
+    password: ""
+  });
 
   function validateForm() {
-    return email.length > 0 && password.length > 0;
+    return fields.email.length > 0 && fields.password.length > 0;
   }
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
+
+    setIsLoading(true);
+
+    try {
+      await Auth.signIn(fields.email, fields.password);
+      userHasAuthenticated(true);
+      history.push("/");
+    } catch (e) {
+      onError(e);
+      setIsLoading(false);
+    }
   }
 
   return (
@@ -23,21 +44,27 @@ export default function Login() {
           <Form.Control
             autoFocus
             type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            value={fields.email}
+            onChange={handleFieldChange}
           />
         </Form.Group>
         <Form.Group size="lg" controlId="password">
           <Form.Label>Password</Form.Label>
           <Form.Control
             type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            value={fields.password}
+            onChange={handleFieldChange}
           />
         </Form.Group>
-        <Button block size="lg" type="submit" disabled={!validateForm()}>
+        <LoaderButton
+          block
+          size="lg"
+          type="submit"
+          isLoading={isLoading}
+          disabled={!validateForm()}
+        >
           Login
-        </Button>
+        </LoaderButton>
       </Form>
     </div>
   );
