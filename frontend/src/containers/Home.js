97f7c258a24932d6470770c7d5e129f8c410bc0d
hoodwinker13 <euhan72@gmail.com>
2022-01-24 15:14:57 -0800

    made a separate frontend folder

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -5,8 +5,8 @@ export default function Home() {
   return (
     <div className="Home">
       <div className="lander">
-        <h1>Scratch</h1>
-        <p className="text-muted">A simple note taking app</p>
+        <h1>Cal Me Maybe</h1>
+        <p className="text-muted">BE SAFE!!</p>
       </div>
     </div>
   );
